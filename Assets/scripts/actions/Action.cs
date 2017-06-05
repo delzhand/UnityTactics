40d7e82ac07a07a94ab0920d9c82b456df67a549
Derek Adams <delzhand@gmail.com>
2017-06-04 21:22:45 -0400

    import actions from json

---
+++ Assets/scripts/actions/Action.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class Action {
+    public string Id;
+    public string CommandSet;
+    public string PSXName;
+    public string PSPName;
+    public string RangeH;
+    public string RangeV;
+    public string Element;
+    public int CTR;
+    public int EffectH;
+    public int EffectV;
+    public int Mod;
+    public int Mp;
+}
